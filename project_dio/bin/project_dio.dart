933350aa02dd363539a6ec1afa1701a9cc434cf2
Fernando Santos <fernandopsilvadossantos@gmail.com>
2023-09-07 14:57:22 -0300

    Projeto adicionado

---
+++ project_dio/bin/project_dio.dart
@@ -0,0 +1,5 @@
+import 'package:project_dio/project_dio.dart' as project_dio;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project_dio.calculate()}!');
+}
---
+++ project_dio/test/project_dio_test.dart
@@ -0,0 +1,8 @@
+import 'package:project_dio/project_dio.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
